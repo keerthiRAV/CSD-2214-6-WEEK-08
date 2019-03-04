cf8cf414121d67c1fdf50c9a01b81a5044cf770f
keerthiRAV <kerthiraveendran93@gmail.com>
2019-03-04 15:41:36 -0500

    COMMIT

--- CSD-2214-6-WEEK08-C0742322/js/main.js
+++ CSD-2214-6-WEEK08-C0742322/js/main.js
@@ -1,1 +1,32 @@
-﻿// Your code here!
+﻿// Your code here!function
+/*function add() {
+            var sum = 0;
+        var i = 1109;
+            while (i >= 119) {
+            sum = sum + i;
+                document.writeln(i);
+        i = i - 8;
+    }
+    document.writeln(i);
+    alert("sum is " + sum);
+
+}*/
+alert("ok";)
+let addnumbers = {
+    starter=1109,
+    stepdown=11,
+    stopnumber=119,
+    adder: function () {
+
+            var sum = 0;
+            var i = 117;
+            while (i >= 11) {
+                sum = sum + i;
+                document.writeln(i);
+            i = i - 8;
+        }
+        document.writeln(i);
+        alert("sum is " + sum);
+    }
+
+}
